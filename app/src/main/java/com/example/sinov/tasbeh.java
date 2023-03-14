b0f5982c7e88c0a53046ebfe1e33cca8a00f817b
DELL <jasurboltaboyev6@gmail.com>
2023-03-14 17:49:02 +0500

    yangisi

---
+++ app/src/main/java/com/example/sinov/tasbeh.java
@@ -0,0 +1,13 @@
+package com.example.sinov;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tasbeh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasbeh);
+    }
+}
